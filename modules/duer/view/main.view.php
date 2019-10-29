cea47a92567e9f4dbc993bdb05fd041db4d69804
Jimmy <jimmy.eoxia@gmail.com>
2019-10-29 14:12:56 +0100

    Ajoutes le nouveau header

--- modules/duer/view/main.view.php
+++ modules/duer/view/main.view.php
@@ -16,36 +16,42 @@ namespace digirisk_dashboard;
 
 defined( 'ABSPATH' ) || exit; ?>
 
-<div id="duer">
-	<div class="wpeo-table table-flex">
-		<div class="table-row table-header">
-			<div class="table-cell table-50" data-title="Ref">Ref</div>
-			<div class="table-cell table-150" data-title="Début">Début</div>
-			<div class="table-cell table-150" data-title="Fin">Fin</div>
-			<div class="table-cell table-100" data-title="Destinataire">Destinataire</div>
-			<div class="table-cell table-100" data-title="Méthodologie">Méthodologie</div>
-			<div class="table-cell table-100" data-title="Sources">Sources</div>
-			<div class="table-cell table-100" data-title="Localisation">Localisation</div>
-			<div class="table-cell table-100" data-title="Notes">Notes</div>
-			<div class="table-cell" data-title="Sites">Sites</div>
-			<div class="table-cell table-100 table-end" data-title="Actions"></div>
-		</div>
-		<?php
-		if ( ! empty( $duers ) ) :
-			foreach ( $duers as $duer ) :
-				\eoxia\View_Util::exec( 'digirisk_dashboard', 'duer', 'item', array(
-					'duer'  => $duer,
-					'sites' => $sites,
+<div class="content-wrap">
+	<?php require PLUGIN_DIGIRISK_DASHBOARD_PATH . '/core/view/main-header.view.php'; ?>
+
+	<div class="wrap wpeo-wrap digirisk-wrap">
+		<div id="duer">
+			<div class="wpeo-table table-flex">
+				<div class="table-row table-header">
+					<div class="table-cell table-50" data-title="Ref">Ref</div>
+					<div class="table-cell table-150" data-title="Début">Début</div>
+					<div class="table-cell table-150" data-title="Fin">Fin</div>
+					<div class="table-cell table-100" data-title="Destinataire">Destinataire</div>
+					<div class="table-cell table-100" data-title="Méthodologie">Méthodologie</div>
+					<div class="table-cell table-100" data-title="Sources">Sources</div>
+					<div class="table-cell table-100" data-title="Localisation">Localisation</div>
+					<div class="table-cell table-100" data-title="Notes">Notes</div>
+					<div class="table-cell" data-title="Sites">Sites</div>
+					<div class="table-cell table-100 table-end" data-title="Actions"></div>
+				</div>
+				<?php
+				if ( ! empty( $duers ) ) :
+					foreach ( $duers as $duer ) :
+						\eoxia\View_Util::exec( 'digirisk_dashboard', 'duer', 'item', array(
+							'duer'        => $duer,
+							'child_sites' => $child_sites,
+						) );
+					endforeach;
+				endif;
+
+				\eoxia\View_Util::exec( 'digirisk_dashboard', 'duer', 'edit', array(
+					'element'     => $new_duer,
+					'child_sites' => $child_sites,
 				) );
-			endforeach;
-		endif;
+				?>
+			</div>
 
-		\eoxia\View_Util::exec( 'digirisk_dashboard', 'duer', 'edit', array(
-			'element' => $new_duer,
-			'sites'   => $sites,
-		) );
-		?>
-	</table>
-
-	<?php \eoxia\View_Util::exec( 'digirisk_dashboard', 'duer', 'edit-modal' ); ?>
+			<?php \eoxia\View_Util::exec( 'digirisk_dashboard', 'duer', 'edit-modal' ); ?>
+		</div>
+	</div>
 </div>
