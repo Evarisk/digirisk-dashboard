3efbb3a08e3d602e47fe2b9ae4b4b654c7a3783d
Jimmy <jimmy.eoxia@gmail.com>
2019-10-29 14:11:08 +0100

    Mise à jour de la vue pour éditer un site

--- modules/site/view/edit.view.php
+++ modules/site/view/edit.view.php
@@ -1,21 +1,3 @@
-<?php
-/**
- * Le template principal pour ajouter un site
- *
- * @author    Evarisk <dev@evarisk.com>
- * @copyright (c) 2017-2018 Evarisk <dev@evarisk.com>.
- *
- * @license   AGPLv3 <https://spdx.org/licenses/AGPL-3.0-or-later.html>
- *
- * @package   DigiRisk_Dashboard\Templates
- *
- * @since     0.2.0
- */
-
-namespace digirisk_dashboard;
-
-defined( 'ABSPATH' ) || exit; ?>
-
 <div class="edit-site">
 	<div class="notice notice-success hidden">
 		<p></p>
@@ -33,47 +15,47 @@ defined( 'ABSPATH' ) || exit; ?>
 		<div class="form-element form-element-required">
 			<span class="form-label">URL du site</span>
 			<label class="form-field-container">
-				<input type="text" class="form-field" name="url" value="<?php echo ! empty( $site ) ? esc_attr( $site['url'] ) : ''; ?>" />
+				<input type="text" class="form-field" name="url" value="<?php echo ! empty( $edit_site ) ? esc_attr( $edit_site['url'] ) : ''; ?>" />
 			</label>
 		</div>
 
 		<div class="form-element form-element-required">
 			<span class="form-label">Clé unique</span>
 			<label class="form-field-container">
-				<input type="text" class="form-field" name="unique_key" value="<?php echo ! empty( $site ) ? esc_attr( $site['unique_key'] ) : ''; ?>" />
+				<input type="text" class="form-field" name="unique_key" value="<?php echo ! empty( $edit_site ) ? esc_attr( $edit_site['unique_key'] ) : ''; ?>" />
 			</label>
 		</div>
 
 		<div class="form-element">
 			<span class="form-label">Authentification User</span>
 			<label class="form-field-container">
-				<input type="text" class="form-field" name="auth_user" value="<?php echo ! empty( $site ) ? esc_attr( $site['auth_user'] ) : ''; ?>" />
+				<input type="text" class="form-field" name="auth_user" value="<?php echo ! empty( $edit_site ) ? esc_attr( $edit_site['auth_user'] ) : ''; ?>" />
 			</label>
 		</div>
 
 		<div class="form-element">
 			<span class="form-label">Authentification Password</span>
 			<label class="form-field-container">
-				<input type="password" class="form-field" name="auth_password" value="<?php echo ! empty( $site ) ? esc_attr( $site['auth_password'] ) : ''; ?>" />
+				<input type="password" class="form-field" name="auth_password" value="<?php echo ! empty( $edit_site ) ? esc_attr( $edit_site['auth_password'] ) : ''; ?>" />
 			</label>
 		</div>
 
 
 		<?php
-		if ( ! empty( $site ) ) :
+		if ( ! empty( $id ) ) :
 			?>
-			<div class="wpeo-button button-main button-disable action-input"
-				data-parent="wpeo-form">
+			<div class="wpeo-button button-main action-input"
+				 data-parent="wpeo-form">
 				<span>Modifier le site</span>
 			</div>
-			<?php
+		<?php
 		else:
 			?>
 			<div class="wpeo-button button-main button-disable action-input"
-				data-parent="wpeo-form">
+				 data-parent="wpeo-form">
 				<span>Ajouter le site</span>
 			</div>
-			<?php
+		<?php
 		endif;
 
 		?>
