bd35e936fc4e55c800f4a8a74539a4cb24efd3a1
Jimmy <jimmy.eoxia@gmail.com>
2019-10-29 14:07:43 +0100

    Ajoutes l'action ajax pour vérifier la connectivité d'un site

--- modules/site/action/class-site.action.php
+++ modules/site/action/class-site.action.php
@@ -22,9 +22,54 @@ class Class_Site_Action {
 	 * @since 0.1.0
 	 */
 	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'callback_admin_menu' ) );
+
+		add_action( 'wp_ajax_sites_child_check_statut', array( $this, 'check_statut' ) );
+
 		add_action( 'wp_ajax_digi_dashboard_edit_site', array( $this, 'ajax_edit_site' ) );
 		add_action( 'wp_ajax_digi_dashboard_load_edit_site', array( $this, 'ajax_load_edit_site' ) );
 		add_action( 'wp_ajax_digi_dashboard_delete_site', array( $this, 'ajax_delete_site' ) );
+	}
+
+	/**
+	 * Ajoutes le menu DigiRisk Dashboard dans l'administration de WordPress.
+	 *
+	 * @since 0.2.0
+	 */
+	public function callback_admin_menu() {
+		add_submenu_page( 'digirisk-dashboard-site', __( 'DigiRisk Dashboard - Ajouter un site', 'digirisk' ), __( 'DigiRisk Dashboard', 'digirisk' ), 'manage_options', 'digirisk-dashboard-add', array( Class_Site::g(), 'display_edit' ) );
+	}
+
+	public function check_statut() {
+		$id = ! empty( $_POST['id'] ) ? (int) $_POST['id'] : 0;
+
+		if ( empty( $id ) ) {
+			wp_send_json_error();
+		}
+
+		$site_key = \eoxia\Config_Util::$init['digirisk_dashboard']->site->site_key;
+		$sites    = get_option( $site_key, array() );
+
+		if ( empty( $sites[ $id ] ) ) {
+			wp_send_json_success( array(
+				'success' => false,
+			) );
+		}
+
+		$site = $sites[ $id ];
+
+		$url = $site['url'] . '/wp-json/digi/v1/statut';
+
+		$site['check_connect'] = Request_Util::post( $url, array(), array(
+			'auth_user'     => $site['auth_user'],
+			'auth_password' => $site['auth_password'],
+		), $site['hash'] );
+
+		wp_send_json_success( array(
+			'success'       => $site['check_connect']->statut,
+			'error_code'    => $site['check_connect']->error_code,
+			'error_message' => $site['check_connect']->error_message,
+		) );
 	}
 
 	/**
@@ -67,16 +112,18 @@ class Class_Site_Action {
 			$api_url = $url . '/wp-json/digi/v1/register-site';
 
 			$data = array(
-				'id'         => $id,
+				'id'         => ! empty( $site_id ) ? $site_id : $last_id,
 				'url'        => $url,
 				'url_parent' => get_site_url(),
 				'unique_key' => $unique_key,
 			);
 
-			$response = Request_Util::g()->post( $api_url, $data, array(
-				'auth_user'     => $auth_user,
-				'auth_password' => $auth_password,
-			) );
+			$response = Request_Util::g()->post( $api_url, $data,
+				array(
+					'auth_user'     => $auth_user,
+					'auth_password' => $auth_password,
+				)
+			);
 
 			if ( $response ) {
 				if ( ! empty( $response->error_code ) ) {
@@ -87,23 +134,22 @@ class Class_Site_Action {
 					}
 				} else {
 					unset( $data['url_parent'] );
-					$data_to_hash   = implode( '', $data );
-					$string_to_hash = hash( 'sha256', $data_to_hash );
+					$data_to_hash = implode( '', $data );
+					$hash         = hash( 'sha256', $data_to_hash );
 
 					$tmp = array(
 						'title'         => $response->title,
 						'url'           => $data['url'],
-						'hash'          => $string_to_hash,
+						'hash'          => $hash,
 						'unique_key'    => $unique_key,
 						'auth_user'     => $auth_user,
 						'auth_password' => $auth_password,
 					);
 
-					if ( ! $already_exist ) {
-
+					if ( ! $already_exist && empty( $site_id ) ) {
 						$sites[ $last_id + 1 ] = $tmp;
 					} else {
-						$sites[ $id ] = $tmp;
+						$sites[ $site_id ] = $tmp;
 					}
 
 					update_option( $site_key, $sites );
@@ -137,13 +183,13 @@ class Class_Site_Action {
 			wp_send_json_error();
 		}
 
-		$sites      = get_option( \eoxia\Config_Util::$init['digirisk_dashboard']->site->site_key, array() );
-		$site = $sites[ $id ];
+		$sites = get_option( \eoxia\Config_Util::$init['digirisk_dashboard']->site->site_key, array() );
+		$site  = $sites[ $id ];
 
 		ob_start();
-		\eoxia\View_Util::exec( 'digirisk_dashboard', 'site', 'edit/main', array(
-			'id'   => $id,
-			'site' => $site,
+		\eoxia\View_Util::exec( 'digirisk_dashboard', 'site', 'edit', array(
+			'id'        => $id,
+			'edit_site' => $site,
 		) );
 		$view = ob_get_clean();
 
