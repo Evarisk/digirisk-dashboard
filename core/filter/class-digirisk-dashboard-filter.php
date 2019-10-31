38a202c714bd097dde5deb4eb38c702ac42c5f6d
Jimmy <jimmy.eoxia@gmail.com>
2019-10-31 10:08:56 +0100

    Gestion des multisites dans l'admin bar personnalisé de DigiRisk Dashboard

--- core/filter/class-digirisk-dashboard-filter.php
+++ core/filter/class-digirisk-dashboard-filter.php
@@ -25,6 +25,10 @@ class Class_Digirisk_Dashboard_Filter {
 	 */
 	public function __construct() {
 		add_filter( 'digi_handle_model_actions_end', array( $this, 'callback_digi_handle_model_actions_end' ), 10, 2 );
+
+		add_filter( 'digirisk_dashboard_main_header_ul_after', array( $this, 'add_header_multisite' ) );
+
+		add_filter( 'http_request_timeout', array( $this, 'upgrade_timeout_request' ) );
 	}
 
 	/**
@@ -44,6 +48,43 @@ class Class_Digirisk_Dashboard_Filter {
 		return $content;
 	}
 
+	public function add_header_multisite( $content ) {
+		$current_site = get_blog_details( get_current_blog_id() );
+
+		$sites = get_sites();
+
+		usort( $sites, function( $a, $b ) {
+			$al = strtolower($a->blogname);
+			$bl = strtolower($b->blogname);
+
+			if ($al == $bl) {
+				return 0;
+			}
+			return ($al > $bl) ? +1 : -1;
+		} );
+
+		if ( ! empty( $sites ) ) {
+			foreach ( $sites as $key => $site ) {
+				if ( ! is_super_admin( get_current_user_id() ) &&
+					( $site->blog_id == $current_site->blog_id
+						|| empty( get_user_meta( get_current_user_id(), 'wp_' . $site->blog_id . '_user_level', true ) ) ) ) {
+					unset( $sites[ $key ] );
+				} else {
+					$sites[$key]->site_info = get_blog_details( $sites[ $key ]->blog_id );
+				}
+			}
+		}
+
+		ob_start();
+		require( PLUGIN_DIGIRISK_DASHBOARD_PATH . '/core/view/header-multisite.view.php' );
+		$content .= ob_get_clean();
+
+		return $content;
+	}
+
+	public function upgrade_timeout_request( $time ) {
+		return 100;
+	}
 }
 
 new Class_Digirisk_Dashboard_Filter();
