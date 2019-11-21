be50f6b6a0adb97dea9a0d7da82f6bb30f904080
Jimmy <jimmy.eoxia@gmail.com>
2019-11-21 11:25:40 +0100

    Ajout de la clé DEV_MODE

--- digirisk_dashboard.php
+++ digirisk_dashboard.php
@@ -23,7 +23,10 @@ namespace digirisk_dashboard;
 DEFINE( 'PLUGIN_DIGIRISK_DASHBOARD_URL', str_replace( '\\', '/', plugins_url( basename( __DIR__ ) ) . '/' ) );
 DEFINE( 'PLUGIN_DIGIRISK_DASHBOARD_DIR', basename( __DIR__ ) );
 DEFINE( 'PLUGIN_DIGIRISK_DASHBOARD_PATH', str_replace( '\\', '/', realpath( plugin_dir_path( __FILE__ ) ) . '/' ) );
+DEFINE( 'PLUGIN_DIGIRSK_DASHBOARD_DEV_MODE', true );
 
-require_once 'core/external/eo-framework/eo-framework.php';
+if ( ! PLUGIN_DIGIRSK_DASHBOARD_DEV_MODE ) {
+	require_once 'core/external/eo-framework/eo-framework.php';
+}
 
 \eoxia\Init_Util::g()->exec( PLUGIN_DIGIRISK_DASHBOARD_PATH, basename( __FILE__, '.php' ) );
