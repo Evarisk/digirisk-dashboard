7fba3d5989fa6cbb00dad2f4160a95455d0d594a
Jimmy <jimmy.eoxia@gmail.com>
2019-11-21 11:49:44 +0100

    Fix fatal

--- core/action/class-digirisk-dashboard-core-action.php
+++ core/action/class-digirisk-dashboard-core-action.php
@@ -11,6 +11,7 @@ namespace digirisk_dashboard;
 
 use digi\Digirisk;
 use eoxia\Custom_Menu_Handler;
+use eoxia\Custom_Menu_Handler as CMH;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -123,6 +124,8 @@ class Class_Digirisk_Dashboard_Action {
 	 */
 	public function callback_admin_menu() {
 		Custom_Menu_Handler::register_container( 'DigiRisk Dashboard', 'DigiRisk Dashboard', 'manage_options', 'digirisk-dashboard' );
+		CMH::add_logo( 'digirisk-dashboard', PLUGIN_DIGIRISK_DASHBOARD_URL . '/core/assets/images/favicon_hd.png', admin_url( 'admin.php?page=digirisk-dashboard' ) );
+
 		Custom_Menu_Handler::register_menu( "digirisk-dashboard", "Mes sites", "Mes sites", "manage_options", "digirisk-dashboard", array( Class_Digirisk_Dashboard_Core::g(), 'display_page' ), 'fa fa-sitemap' );
 		Custom_Menu_Handler::register_others_menu( 'others', 'digirisk', __( 'DigiRisk Dashboard', 'digirisk' ), __( 'DigiRisk Dashboard', 'digirisk' ), 'read', 'digirisk-dashboard', array( Class_Digirisk_Dashboard_Core::g(), 'display_page' ), 'fa fa-sitemap', 'bottom' );
 
